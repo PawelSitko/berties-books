1798946b4d5f015b30321bbdec616e7647badc93
PawelSitko <pawelsitko90@gmail.com>
2025-11-12 23:03:33 +0000

    finished task 21

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,9 @@
 const express = require("express")
 const router = express.Router()
 
+
+
+
 router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
@@ -10,6 +13,18 @@ router.get('/search-result', function (req, res, next) {
     //searching in the database
     res.send("You searched for: " + req.query.keyword)
 });
+
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.send(result)
+    });
+});
+
 
 // Export the router object so index.js can access it
 module.exports = router
