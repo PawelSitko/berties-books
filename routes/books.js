8013fdd2a1783be8d3ca97adb4b5ac20ce8a7d64
PawelSitko <pawelsitko90@gmail.com>
2025-11-13 00:17:29 +0000

    finished tasks

--- routes/books.js
+++ routes/books.js
@@ -1,30 +1,98 @@
-// Create a new router
-const express = require("express")
-const router = express.Router()
+// routes/books.js
 
+const express = require('express');
+const router = express.Router();
 
+/* 
+  Route: /books/list
+  Purpose: Show all books from the database
+*/
+router.get('/list', (req, res) => {
+  const sqlquery = "SELECT * FROM books";
 
+  db.query(sqlquery, (err, result) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.send("Database error");
+    }
 
-router.get('/search',function(req, res, next){
-    res.render("search.ejs")
+    res.render("list.ejs", { availableBooks: result });
+  });
 });
 
-router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+/* 
+  Route: /books/addbook
+  Purpose: Display a form to add a new book
+*/
+router.get('/addbook', (req, res) => {
+  res.render("addbook.ejs");
 });
 
-router.get('/list', function(req, res, next) {
-    let sqlquery = "SELECT * FROM books"; // query database to get all the books
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.send(result)
-    });
+/* 
+  Route: /books/bookadded
+  Purpose: Insert a new book into the database
+*/
+router.post('/bookadded', (req, res, next) => {
+  const sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+  const newrecord = [req.body.name, req.body.price];
+
+  db.query(sqlquery, newrecord, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(
+      'This book has been added:<br><br>' +
+      'Name: ' + req.body.name + '<br>' +
+      'Price: ' + req.body.price
+    );
+  });
 });
 
+/* 
+  Route: /books/bargainbooks
+  Purpose: Show books with price < £20
+*/
+router.get('/bargainbooks', (req, res) => {
+  const sqlquery = "SELECT * FROM books WHERE price < 20";
 
-// Export the router object so index.js can access it
-module.exports = router
+  db.query(sqlquery, (err, result) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.send("Database error");
+    }
+
+    res.render("list.ejs", { availableBooks: result });
+  });
+});
+
+/* 
+  Route: /books/search
+  Purpose: Show the book search form
+*/
+router.get('/search', (req, res) => {
+  res.render("booksearch.ejs");
+});
+
+/* 
+  Route: /books/searchresult
+  Purpose: Show search results (partial match using LIKE)
+*/
+router.get('/searchresult', (req, res) => {
+  const searchTerm = '%' + req.query.keyword + '%';
+  const sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+
+  db.query(sqlquery, [searchTerm], (err, result) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.send("Database error");
+    }
+
+    res.render("list.ejs", { availableBooks: result });
+  });
+});
+
+/* 
+  Export router so index.js can access it
+*/
+module.exports = router;
